8b5f25548f6a8e54bdf81db2d1833844abd6544f
Flavio <flavio@aiello.ch>
2017-06-30 21:27:36 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for SERVICE in ${SERVICES}; do
-    echo "*** Waiting for service ${SERVICE%:*} port ${SERVICE#*:} with timeout ${TIMEOUT:-60} ***"
-    timeout -t ${TIMEOUT:-60} sh -c -- "while ! nc -z ${SERVICE%:*} ${SERVICE#*:}; do sleep 1; done" || exit "$?"
-done
